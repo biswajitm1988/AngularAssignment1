9dd93b74fa1c2c1486147ca5df4414446f80e11a
Biswajit Mohapatra <Biswajit.Mohapatra@rci.com>
2018-10-24 14:45:02 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HighlightsComponent } from './highlights/highlights.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { HomeComponent } from './home/home.component';
+
+
+const routeMappings: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'highlights',
+    component: HighlightsComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  },
+  {
+    path: 'contactMe',
+    component: ContactMeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightsComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ContactMeComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routeMappings)
   ],
   providers: [],
   bootstrap: [AppComponent]
